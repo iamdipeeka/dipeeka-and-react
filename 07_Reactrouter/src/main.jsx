81c263fc7ecea73be4726cb6d6bdcdebe06a18ff
iamdipeeka <dipeekabaranwal@gmail.com>
2024-08-30 16:03:57 +0530

    reactrouter

--- 07_Reactrouter/src/main.jsx
+++ 07_Reactrouter/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/home/Home.jsx'
+import About from './components/about/About.jsx'
+import Contact from './components/contact/Contact.jsx'
+
+const router = createBrowserRouter([{
+  path:'/',
+  element:<Layout/>,
+  children:[{
+    
+      path:'home',
+      element:<Home/>
+  },
+  {
+    path:'about',
+    element:<About/>
+},
+  {
+     path:'contact',
+     element:<Contact/>
+  }
+]
+}])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+   <RouterProvider router={router}/>
+
 )
